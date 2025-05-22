066626df7d51fe3fb7fb880714f4fbd686cf6d60
jk881 <paculbajames5@gmail.com>
2025-05-22 21:30:29 +0800

    app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import joblib
 
 # Load the trained model and scaler
 model = joblib.load("Logistic_Regression_model.pkl")
+scaler = joblib.load("scaler.pkl")  
 
 st.title("Breast Cancer Prediction App")
 
